0780fc5139579d2efbd34ebfecba37be76ea62a4
luca801238 <24095370@student.hhs.nl>
2025-04-15 15:14:13 +0200

    bug fix + issues aanmaken

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,7 +7,7 @@ import java.util.Locale;
 public class main {
     private static final String URL = "jdbc:mysql://localhost:3306/new_schema";
     private static final String USER = "root";
-    private static final String PASSWORD = "School123!";
+    private static final String PASSWORD = "wachtwoord123";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
